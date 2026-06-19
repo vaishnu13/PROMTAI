c47aa9b1656ccee5f711ba13844fa88fef698289
vaishnu13 <vaishnu7070@gmail.com>
2026-06-19 21:40:54 +0530

    feat: update background video in CinematicSection

--- src/components/CinematicSection.tsx
+++ src/components/CinematicSection.tsx
@@ -88,7 +88,7 @@ export default function CinematicSection() {
       <div className="absolute inset-0 w-full h-full">
         <video
           ref={videoRef}
-          src="https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260328_115001_bcdaa3b4-03de-47e7-ad63-ae3e392c32d4.mp4"
+          src="https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260418_080021_d598092b-c4c2-4e53-8e46-94cf9064cd50.mp4"
           muted
           playsInline
           className="absolute inset-0 w-full h-full object-cover translate-y-[17%]"
